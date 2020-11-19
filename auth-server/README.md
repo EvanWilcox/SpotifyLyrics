9fafa148de70fa306afef65288db0a2d8743fe8d
Evan Wilcox <evanbradleywilcox@gmail.com>
2020-11-18 21:57:50 -0600

    init

---
+++ auth-server/README.md
@@ -0,0 +1,13 @@
+# SpotifyAuthorization Server
+
+## Installation
+
+    npm install
+
+## Start Scripts
+
+In the project directory, you can run:
+
+    node app.js
+
+Starts the Auth Server on [http://localhost:8888](http://localhost:8888). Navigate to [http://localhost:8888/login](http://localhost:8888/login) to be automatically routed to Spotify's Login page.
---
+++ client/src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import "./App.css";
+import { getLyrics } from "genius-lyrics-api";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+class App extends Component {
+  constructor() {
+    super();
+
+    const params = this.getHashParams();
+    const token = params.access_token;
+
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: "", artist: "", albumArt: "", lyrics: "" },
+    };
+
+    if (this.state.loggedIn) {
+      this.timer = setInterval(() => this.getNowPlaying(), 1000);
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    const { nowPlaying } = this.state;
+
+    spotifyApi.getMyCurrentPlaybackState().then((response) => {
+      if (response.item.name !== nowPlaying.name) {
+        const options = {
+          apiKey: process.env.REACT_APP_GENIUS_API_KEY,
+          title: response.item.name,
+          artist: response.item.artists[0].name,
+          optimizeQuery: true,
+        };
+
+        getLyrics(options).then((lyrics) =>
+          this.setState({
+            nowPlaying: {
+              name: response.item.name,
+              artist: response.item.artists[0].name,
+              albumArt: response.item.album.images[0].url,
+              lyrics: lyrics,
+            },
+          })
+        );
+
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  render() {
+    const { loggedIn, nowPlaying } = this.state;
+    const lyrics = nowPlaying.lyrics == null ? ["Lyrics Not Found"] : nowPlaying.lyrics.split(/\r?\n/);
+
+    return (
+      <div className="App">
+        {loggedIn ? (
+          <div>
+            <div className="lyrics">
+              {lyrics.map(function (lyric, index) {
+                return lyric === "" ? <br key={index} /> : <p key={index}>{lyric}</p>;
+              })}
+            </div>
+            <div className="artist">
+              <p className="song-title">{nowPlaying.name}</p>
+              <p className="song-artist">{nowPlaying.artist}</p>
+              <img className="album-art" src={nowPlaying.albumArt} alt="" />
+            </div>
+          </div>
+        ) : (
+          <div className="log-in">
+            <a href="http://localhost:8888/login" className="log-in-button">
+              Login to Spotify
+            </a>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
